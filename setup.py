3f4eefb9b44e837742ea0fd2b4f8f23a5b63e311
Rui  Vieira <ruidevieira@googlemail.com>
2018-05-10 15:21:25 +0100

    Release 0.0.10.
    Update docs.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='pssm',
     packages=['pssm'],
-    version='0.0.9',
+    version='0.0.10',
     description='Python library for state-space models',
     author='Rui Vieira',
     author_email='ruidevieira@googlemail.com',
     url='https://github.com/ruivieira/python-ssm',
     download_url='https://github.com/'
-                 'ruivieira/python-ssm/archive/0.0.9.tar.gz',
+                 'ruivieira/python-ssm/archive/0.0.10.tar.gz',
     keywords=['ssm', 'statistics', 'scientific', 'state-space', 'models'],
     classifiers=[],
 )
