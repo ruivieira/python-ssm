d1738dbd26b7ea0eadecf2d6d2c6033e99ddbd22
Rui Vieira <ruidevieira@googlemail.com>
2020-10-03 22:04:07 +0100

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name="pssm",
     packages=["pssm"],
-    version="0.0.16",
+    version="0.1.0",
     description="Python library for state-space models",
     author="Rui Vieira",
     author_email="ruidevieira@googlemail.com",
-    url="https://github.com/ruivieira/python-ssm",
-    download_url="https://github.com/" "ruivieira/python-ssm/archive/0.0.16.tar.gz",
+    url="https://git.sr.ht/~ruivieira/python-ssm",
+    download_url="https://git.sr.ht/~ruivieira/python-ssm/archive/0.1.0.tar.gz",
     keywords=["ssm", "statistics", "scientific", "state-space", "models"],
     classifiers=[],
 )
